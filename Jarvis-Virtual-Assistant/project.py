888b599832834fe9e6518096ca1d4119998accdf
Minhaj Tkc <minhajtkc@gmail.com>
2024-10-04 16:38:27 +0530

    google recognization added

--- Jarvis-Virtual-Assistant/project.py
+++ Jarvis-Virtual-Assistant/project.py
@@ -13,3 +13,16 @@ def speak(text):
 
 if __name__ == "__main__":
     speak("Initializing Jarvis....")
+    while True:
+        r = sr.Recognizer()
+
+        print("recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=2, phrase_time_limit =1)
+                
+            command = r.recognize_google(audio)
+            print(command)
+        except Exception as e:
+            print("Error: {0}".format(e))
