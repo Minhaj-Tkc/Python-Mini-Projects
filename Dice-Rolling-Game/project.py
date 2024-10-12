c959e7dd0270f559a94aa4b138577c45260c3694
Minhaj Tkc <minhajtkc@gmail.com>
2024-10-12 14:04:44 +0530

    Dice Rolling Game Added

---
+++ Dice-Rolling-Game/project.py
@@ -0,0 +1,13 @@
+from random import randint
+
+while True:
+    choice = input('Roll the dice? (y/n): ').lower()
+    if choice == 'y':
+        die1 = randint(1, 6)
+        die2 = randint(1, 6)
+        print(f'({die1}, {die2})')
+    elif choice == 'n':
+        print('Thanks for playing!')
+        break
+    else:
+        print("Invalid choice!")
